fb5b90e65723656b8dd57008a1c88e15164b2199
DootLord <shaunoman840@gmail.com>
2025-02-13 16:31:02 +0000

    Removed server routing from vite.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3012,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()]
 })
